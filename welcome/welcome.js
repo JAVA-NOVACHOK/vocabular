1ed9391c216d9ce8b833df6f8c796235692c0db7
Artem Nikiforov <anikiforov@lenovo.com>
2023-05-26 18:51:30 +0300

    Dropdown is good

--- welcome/welcome.js
+++ welcome/welcome.js
@@ -1,11 +1,31 @@
+// $(document).ready(function () {
 
 
 var addButton = document.getElementById("addWordsBulk");
+// var dropdown = document.getElementById("lessonDropdown")
 
 addButton.addEventListener("click", function () {
-    window.open("../add-csv/manage-words.html", "_blank");
+    window.open("../add-csv/manage-words.html");
 });
 
-addButton.addEventListener("click", function () {
-    window.open("../add-csv/manage-words.html", "_blank");
+// addButton.addEventListener("click", function () {
+//     window.open("../add-csv/manage-words.html", "_blank");
+// });
+
+document.addEventListener('DOMContentLoaded', function () {
+    $('#lessonDropdown').select2({
+        tags: true,
+        createTag: function (params) {
+            return {
+                id: params.term,
+                text: params.term,
+                isNew: true
+            };
+        },
+        placeholder: "Select an option or type your own",
+        allowClear: true
+    });
 });
+
+
+// })
